81345ba8d7ff40e10d9aeb7f423a9694c07f39c3
Grigory <grishavinyar@icloud.com>
2022-06-22 16:51:00 +0300

    Created the project

---
+++ All patterns together/All patterns together/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  All patterns together
+//
+//  Created by Григорий Виняр on 22/06/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .orange
+    }
+
+
+}
+
